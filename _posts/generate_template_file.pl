03c327e8f5e76c0a9f4e9f2dbd9b6e2dd375c035
Hao Wu <hwu@intellisurvey.com>
2015-03-27 12:13:28 -0700

    typo

--- _posts/generate_template_file.pl
+++ _posts/generate_template_file.pl
@@ -1,14 +1,18 @@
 use v5.20;
 use POSIX qw(strftime);
+if (not defined $ARGV[0]) {
+    die "please give the tile"
+
+}
 my $date = strftime "%Y-%m-%d", localtime;
 my $time = strftime "%Y-%m-%d %H:%M", localtime;
-
-my $fh_file = IO::File->new($date . '-' .$ARGV[0] . '.md', "w+");
+my $title = $ARGV[0] =~s[\s][\-]r;
+my $fh_file = IO::File->new($date . '-' . $title . '.md', "w+");
 
 my $template = <<TEMPLATE;
 ---
 layout: post
-title:  todo  
+title:  $title 
 date:   $time 
 categories: tech 
 ---
