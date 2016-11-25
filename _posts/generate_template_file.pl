a8e912b14b92e2531356b715c1e1db94f8577f26
Hao Wu <hwu@intellisurvey.com>
2016-11-24 20:13:01 -0800

    small fix

--- _posts/generate_template_file.pl
+++ _posts/generate_template_file.pl
@@ -3,13 +3,11 @@ use v5.18;
 use POSIX qw(strftime);
 if ( not defined $ARGV[0] ) {
     die "please give the tile"
-
 }
 my $date = strftime "%Y-%m-%d",       localtime;
 my $time = strftime "%Y-%m-%d %H:%M", localtime;
 my $title = $ARGV[0] =~ s[\s][\-]rg;
 my $fh_file = IO::File->new( $date . '-' . $title . '.md', "w+" );
-
 my $template = <<TEMPLATE;
 ---
 layout: post
